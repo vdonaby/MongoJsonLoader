42a99738b5c0b47d873add99c1d198a647037dd5
Vontez <vontez.donaby@target.com>
2017-03-01 15:40:16 -0600

    adding insert for filings

--- src/main/java/com/msse/bigdata/IrsformsApplication.java
+++ src/main/java/com/msse/bigdata/IrsformsApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,26 +33,28 @@ public class IrsformsApplication {
 		JSONParser parser = new JSONParser();
 
 		List<String> urls = new ArrayList<>();
+		List<String> filingDocuments = new ArrayList<>();
 
-		Integer counter = 0;
+		Integer numberOfFilings = 0;
 
 		try {
 
 			parser = new JSONParser();
-			Object obj = parser.parse(new FileReader("/2015.json"));
+			Object obj = parser.parse(new FileReader("/Users/z001hk8/Desktop/UOFM/2017/BigData/IRSData/2015.json"));
 			JSONObject jsonObject = (JSONObject) obj;
-			JSONArray filings = (JSONArray) jsonObject.get("Filings2015");
+			JSONArray indexes = (JSONArray) jsonObject.get("Filings2015");
 
-			for(Object filing : filings) {
+			for(Object index : indexes) {
 				document = new BasicDBObject();
-				JSONObject jsonNumber = (JSONObject) filing;
+				JSONObject jsonNumber = (JSONObject) index;
 				String ein = (String) jsonNumber.get("EIN");
 				String url = (String) jsonNumber.get("URL");
 				urls.add(url);
 				document.put(ein, jsonNumber.toString());
 				collection.insert(document);
-				counter++;
+				numberOfFilings++;
 			}
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -59,14 +65,29 @@ public class IrsformsApplication {
 		System.out.println(urls.size());
 
 		for(String urlLink: urls) {
-
-
-
+			StringBuilder result = new StringBuilder();
+			URL url = new URL(urlLink);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			rd.close();
+			filingDocuments.add(result.toString());
 		}
 
+		//TODO Convert XML document to JSON
 
-
-
+		for(Object filingDocument : filingDocuments) {
+			document = new BasicDBObject();
+			JSONObject jsonNumber = (JSONObject) filingDocument;
+			String ein = (String) jsonNumber.get("EIN");
+			document.put(ein, jsonNumber.toString());
+			collection.insert(document);
+			numberOfFilings++;
+		}
 
 	}
 }
